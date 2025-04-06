11ac0ce97b2bd69addd51839a540b3afa66e4d27
ArmJav <91393155+ArmJav@users.noreply.github.com>
2025-04-06 15:14:15 +0300

    ss

--- backend/functions.py
+++ backend/functions.py
@@ -15,7 +15,7 @@ def generate_random_statistics(n: int) -> list[list]:
 
 # Функция для расчета вероятности каждого места для каждого участника
 def get_probability_vector() -> dict:
-    history = get_race_history()
+    history = generate_random_statistics()
     n = len(history)
     probs = {"pl1": [0, 0, 0, 0, 0, 0], "pl2": [0, 0, 0, 0, 0, 0], "pl3": [0, 0, 0, 0, 0, 0], "pl4": [0, 0, 0, 0, 0, 0],
              "pl5": [0, 0, 0, 0, 0, 0], "pl6": [0, 0, 0, 0, 0, 0]}
